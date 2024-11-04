3b2eab4a2a41d05b801964c90709f658666532ad
MiguelValencia <valenciaortiz364@gmail.com>
2024-11-04 15:33:37 -0500

    abordamos la sesion 2 hasta el paso 17 slide 37

---
+++ Sesion_2/main.py
@@ -0,0 +1,8 @@
+# Importa la libreria Pandas, fundamental para analisis de datos
+import pandas as pd
+# Define la ruta del archivo CSV que contiene los datos
+# Si el archivo esta en el mismo directorio, basta con poner el nombre del archivo
+path = 'Online_Retail.csv'
+retail_data = pd.read_csv(path, encoding='latin1')
+print(type(retail_data))
+print(retail_data)
