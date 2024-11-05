1f0b9ae4a5eef8bd16879c3cdaee3fecfcc17313
MiguelValencia <valenciaortiz364@gmail.com>
2024-11-04 20:14:23 -0500

    viento a;adido

--- Sesion_2/main.py
+++ Sesion_2/main.py
@@ -2,7 +2,9 @@
 import pandas as pd
 # Define la ruta del archivo CSV que contiene los datos
 # Si el archivo esta en el mismo directorio, basta con poner el nombre del archivo
-path = 'Online_Retail.csv'
+path = 'estaciones_viento.csv'
+
 retail_data = pd.read_csv(path, encoding='latin1')
+
 print(type(retail_data))
 print(retail_data)
